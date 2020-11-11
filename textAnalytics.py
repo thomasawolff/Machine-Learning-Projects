1cd3536cdbb7ba2a81000e1938a8074ebc375d80
Thomas Wolff <datawolff@gmail.com>
2020-11-11 14:32:42 -0700

    Add files via upload

--- textAnalytics.py
+++ textAnalytics.py
@@ -7,8 +7,11 @@ import pandas as pd
 import seaborn as sns
 from nltk import ne_chunk
 from textblob import TextBlob
+from sklearn.cluster import KMeans
+import scipy.cluster.hierarchy as sch
 from multiprocessing import Pool
 import matplotlib.pyplot as plt
+from sklearn.neighbors import KernelDensity
 from scipy.stats import norm
 from nltk.corpus import stopwords
 from nltk.stem import wordnet
@@ -16,6 +19,7 @@ from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 from collections import Counter
 from nltk.tokenize import word_tokenize
+from sklearn.cluster import AgglomerativeClustering
 from sklearn.feature_extraction.text import CountVectorizer
 
 
@@ -139,7 +143,7 @@ class textAnalytics(object):
         sub = []
         self.comm = self.review_df 
         self.comm = self.comm.sample(100)
-        for i in comm.commentText.values:
+        for i in self.comm.commentText.values:
             try:
                 analysis = TextBlob(i)
                 pol.append(round(analysis.sentiment.polarity,2))
@@ -157,26 +161,93 @@ class textAnalytics(object):
         self.comm['subjectivity']=sub
         #comm.to_csv('youTubeVideosSentimentAnalysisSample.csv',sep=',',encoding='utf-8')
         print(self.comm)
-
-        plt.grid(axis='y', alpha=0.50)
-        plt.title('Histogram of comment sentiment')
-        plt.xlabel('Sentiment Scores')
-        plt.ylabel('Frequency')
-        sns.distplot(self.comm['polarity'],hist=False,fit=norm,kde=True,norm_hist=True)
-        plt.show()
-
-        plt.grid(axis='y', alpha=0.50)
-        plt.title('Histogram of comment sentiment')
-        plt.xlabel('Sentiment Scores')
-        plt.ylabel('Frequency')
-        sns.distplot(self.comm['subjectivity'],hist=False,fit=norm,kde=True,norm_hist=True)
-        plt.show()
         ##                    videoID       categoryID  views  ...    replies  polarity   subjectivity
         ##          251449  LLGENw4C1jk          17   1002386  ...      0.0      0.50          0.50
         ##          39834   3VVnY86ulA8          22    802134  ...      0.0      0.00          0.10
         ##          203460  iA86imHKCMw          17   3005399  ...      0.0     -0.08          0.69
         ##          345225  RRkdV_xmYOI          23    367544  ...      0.0      0.13          0.76
         ##          402953  vQ3XgMKAgxc          10  51204658  ...      0.0      0.25          0.50
+
+
+    def distPlotter(self):
+        self.sentimentAnalysis()
+        plt.grid(axis='y', alpha=0.50)
+        plt.title('Histogram of comment sentiment')
+        plt.xlabel('Sentiment Scores')
+        plt.ylabel('Frequency')
+        sns.distplot(self.comm['polarity'],hist=False,fit=norm,kde=True,norm_hist=True)
+        #x,y = sns.kdeplot(self.comm['polarity']).get_lines()[0].get_data()
+        plt.show()
+
+        plt.grid(axis='y', alpha=0.50)
+        plt.title('Histogram of comment subjectivity')
+        plt.xlabel('Subjectivity Scores')
+        plt.ylabel('Frequency')
+        #sns.distplot(self.comm['subjectivity'],hist=False,fit=norm,kde=True,norm_hist=True)
+        plt.show()
+
+
+
+class clustering(object):
+
+   # choose columns you want to cluster from dataset when instantiating class
+   def __init__(self,column1,column2,file):
+      self.column1 = column1
+      self.column2 = column2
+      self.file = file
+      self.datasetNew = pd.read_csv(self.file)
+      self.X = self.datasetNew.iloc[:,[self.column1,self.column2]].values
+      
+   def dendrogram(self,linkage):
+      # using dendrogram to optimal number of clusters
+      dendrogram = sch.dendrogram(sch.linkage(self.X,linkage))
+      plt.title('Dendrogram')
+      plt.xlabel('X-Value')
+      plt.ylabel('Y-Value')
+      plt.show()
+      
+   def agglomViz(self,affin,link,clusters):
+      # fitting heiarchical clustering to the dataset
+      hc = AgglomerativeClustering(n_clusters = clusters, affinity = affin, linkage = link)
+      y_hc = hc.fit_predict(self.X)
+      
+      # visualizing data using agglomerative method
+      for i in range(0,clusters):
+         plt.scatter(self.X[y_hc == i, 0], self.X[y_hc == i, 1], s = 100)
+      plt.show()
+      plt.title('Clusters of Data: Agglomerative Method')
+      plt.xlabel('X-Value')
+      plt.ylabel('Y-Value')
+      plt.legend()
+      plt.show()
+      
+   def kMeansElbow(self):
+      # using the elbow method to find optimal number of clusters
+      wcss = []
+      for i in range(1, 11):
+         kmeans = KMeans(n_clusters = i, init = 'k-means++', max_iter=300,n_init=10,random_state=0)
+         kmeans.fit(self.X)
+         wcss.append(kmeans.inertia_)   
+      plt.plot(range(1, 11),wcss)
+      plt.title('The Elbow Method')
+      plt.xlabel('Number of Data')
+      plt.ylabel('WCSS')
+      plt.show()
+      
+   def kMeansViz(self,clusterNum):
+      # applying k means to the dataset
+      kmeans = KMeans(n_clusters = clusterNum, init = 'k-means++',max_iter=300,n_init=10,random_state=0)
+      y_kmeans = kmeans.fit_predict(self.X)
+      
+      # visualizing the clusters using K-means
+      for i in range(0,clusterNum):
+         plt.scatter(self.X[y_kmeans == i, 0], self.X[y_kmeans == i, 1], s = 100)
+         plt.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],s=50,c='yellow',label='Centroids')
+      plt.title('Clusters of Data: K-Means Method')
+      plt.xlabel('X-Value')
+      plt.ylabel('Y-Value')
+      plt.show()
+
 
 
 url = (r'C:\Users\moose_f8sa3n2\Google Drive\Research Methods\Course Project\YouTube Data\Unicode Files\youTubeVideosUTF.csv')
@@ -211,9 +282,35 @@ go = textAnalytics(url)
 ##    Pool(go.tagsMaker())
 
 ##if __name__ == '__main__':
-##    Pool(go.computeIDF())
+##    Pool(go.sentimentAnalysis())
 
 if __name__ == '__main__':
-    Pool(go.sentimentAnalysis())
+    Pool(go.distPlotter())
+
+
+
+first_column = 3
+second_column = 4
+number_clusters = 5
+affinity = 'euclidean'
+linkage = 'ward'
+
+
+### create the dendrogram for hierarchical method
+##hc1 = clustering(first_column,second_column,url)
+##hc1.dendrogram(linkage)
+##
+### displays the elbow method for determining custer number
+##elbow = clustering(first_column,second_column,url)
+##elbow.kMeansElbow()
+##
+### create the agglomorative hierarchical cluster
+##hc2 = clustering(first_column,second_column,url)
+##hc2.agglomViz(affinity,linkage,number_clusters)
+##
+### create the K-Means cluster with yellow centroids
+##kMeans = clustering(first_column,second_column,url)
+##kMeans.kMeansViz(number_clusters)
+    
 
 
